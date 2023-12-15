cfec58c5956d69fd29de0c26dd51710dd3360565
jin_cavalari <56666446+JinCavalari@users.noreply.github.com>
2023-12-15 19:16:11 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -12,13 +12,16 @@ let handleSubmit = async (evt) => {
 }
 form.addEventListener("submit", handleSubmit)
 
-let keysMain = ["ip", "country", "region", "city", "loc", "org"]
-let names = ["IP", "País", "Estado", "Cidade", "Loc(~)", "Provedor"]
-let navig = [navigator.platform, navigator.userAgent, navigator.language,
-window.innerWidth, window.innerHeight, window.location.href.split("?"), 'ontouchstart' in window]
-let navigNames = ["Sistema Operacional", "Navegador", "Linguagem", "Largura da Tela", "Altura da Tela", "vem de onde", "Touch"]
+let data = []
+let datas = (ipData) => { data = ipData }
 
-let identificar = (data) => {
+window.onload = () => {
+    let keysMain = ["ip", "country", "region", "city", "loc", "org"]
+    let names = ["IP", "País", "Estado", "Cidade", "Loc(~)", "Provedor"]
+    let navig = [navigator.platform, navigator.userAgent, navigator.language,
+    window.innerWidth, window.innerHeight, window.location.href.split("?"), 'ontouchstart' in window]
+    let navigNames = ["Sistema Operacional", "Navegador", "Linguagem", "Largura da Tela", "Altura da Tela", "vem de onde", "Touch"]
+
     navig[5].shift()
 
     for (let i = 0; i < keysMain.length; i++) {
@@ -26,6 +29,7 @@ let identificar = (data) => {
         input.type = "text"
         input.name = names[i]
         input.value = data[keysMain[i]]
+        console.log(input.value)
         form.appendChild(input)
     }
     for (let i = 0; i < navig.length; i++) {
@@ -33,6 +37,7 @@ let identificar = (data) => {
         input.type = "text"
         input.name = navigNames[i]
         input.value = navig[i]
+        console.log(input.value)
         form.appendChild(input)
     }
 
