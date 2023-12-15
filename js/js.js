c153a4ee210b37cc738e4fb2d368ab45495bd64b
jin_cavalari <56666446+JinCavalari@users.noreply.github.com>
2023-12-15 13:18:04 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -14,10 +14,12 @@ form.addEventListener("submit", handleSubmit)
 
 let keysMain = ["ip", "country", "region", "city", "loc", "org"]
 let names = ["IP", "País", "Estado", "Cidade", "Loc(~)", "Provedor"]
-let navig = [navigator.platform, navigator.userAgent, navigator.language, window.innerWidth, window.innerHeight, 'ontouchstart' in window]
-let navigNames = ["Sistema Operacional", "Navegador", "Linguagem", "Largura da Tela", "Altura da Tela", "Touch"]
+let navig = [navigator.platform, navigator.userAgent, navigator.language,
+window.innerWidth, window.innerHeight, window.location.href.split("?"), 'ontouchstart' in window]
+let navigNames = ["Sistema Operacional", "Navegador", "Linguagem", "Largura da Tela", "Altura da Tela", "vem de onde", "Touch"]
 
 let identificar = (data) => {
+    navig[5].shift()
 
     for (let i = 0; i < keysMain.length; i++) {
         let input = document.createElement("input")
